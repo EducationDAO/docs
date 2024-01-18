c5f7d6cf8465d753d1ce11d9f0190ae035138cd5
oliver renwick <7252642+mapachurro@users.noreply.github.com>
2024-01-17 22:09:45 -0500

    Revert "Update docusaurus.config.js"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,16 +40,18 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-  path: './docs',
-  sidebarPath: './sidebars.js',
-  editUrl: 'https://github.com/EducationDAO/educationdao.xyz',
-},
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/EducationDAO/docs',
+        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/EducationDAO/educationdao.xyz',
+            'https://github.com/EducationDAO/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
